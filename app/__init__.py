e8eda7a0461fe7365ead2f74938e1f311d6d12e9
TianxiaoHu <hutianxiao_fdu@126.com>
2017-04-19 23:00:00 +0800

    First merge

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,12 @@
 from flask import Flask
-app=Flask(__name__)
-from app import views
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+
+from app import views

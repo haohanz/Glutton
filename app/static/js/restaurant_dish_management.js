704cd9cd38107105a57429910a6cd8605d9a1b4e
hh1680651 <591184145@qq.com>
2017-04-21 23:59:41 +0800

    restaurant_dish_management.js function update

--- app/static/js/restaurant_dish_management.js
+++ app/static/js/restaurant_dish_management.js
@@ -4,6 +4,7 @@
 
 
 $(document.ready(function(){
+	var dish_id = "";
 	var url = location.search;
     if (url.indexOf("?") != -1) {
         var str = url.substr(1);
@@ -12,20 +13,29 @@ $(document.ready(function(){
         for (var i = 0; i < str_splits.length; i++) {
         	url_vals[str_splits[i].split("=")[0]] = str_splits[i].split("=")[1];
         }
-        var customer_id = url_vals["customer_id"];
-        $.get("/get_user_history",{"customer_id":customer_id},function(data){
+        var restaurant_id = url_vals["restaurant_id"];
+        if(url_vals["dish_id"]){
+        	dish_id = url_vals["dish_id"];
+    	}
+        $.get("/get_restaurant_orders",url_vals,function(data){  //search for orders with dish_id init, {"restaurantid":,"dish_id":} or {"restaurant_id":.} which means view history
         	alert("get data!"+data);
         	var str= '';
         	$.each(data,function(i,item){
-        		var restaurant_name = item.restaurant_name;
+        		var restaurant_name = item.customer_id;
+        		var create_time = item.create_time;
         		var order_id = item.order_id;
+        		var receive_time = item.receive_time;
         		var order_total_price = item.order_total_price;
         		var dishes = item.dishes;
         		var dish_list = '<ul class="list-style-none lh-condensed">';
         		$.each(dishes, function(i, dish_item){
         			var dish_name = dish_item.dish_name;
         			var dish_price = dish_item.dish_price;
-					dish_list += '<li class="mb-1">'+ dish_name+'<span class="default-currency">¥'+dish_price+'</span></li>'+;
+        			if (url_vals["dish_id"] && dish_item.dish_id == dish_id){
+						dish_list += '<li class="mb-1" style="color: red;">'+ dish_name+'<span class="default-currency">¥'+dish_price+'</span></li>'+;
+        			} else {
+						dish_list += '<li class="mb-1">'+ dish_name+'<span class="default-currency">¥'+dish_price+'</span></li>'+;
+					}
         		});
         		dish_list += '</ul>';
         		if (i%3 == 0) {
@@ -37,7 +47,7 @@ $(document.ready(function(){
         		str += '\
 					 <div class="plans-card col-12 col-sm-8 col-md-4 text-center bg-white border rounded-2">\
 					    <a class="d-block mb-0 py-2 px-3 border-bottom text-green no-underline">\
-					      <h2 class="alt-h3">'+restaurant_name+'</h2>\
+					      <h2 class="alt-h3">user:'+restaurant_name+'</h2>\
 					    </a>\
 					  <div class="plans-card-text p-3">\
 					    <h3 class="alt-h2 text-normal mb-0 lh-condensed">\

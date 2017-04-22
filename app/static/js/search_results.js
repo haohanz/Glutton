b9998bdd105f25c2145d78466a7419b8cba8a1db
hh1680651 <591184145@qq.com>
2017-04-22 10:32:48 +0800

    change the layouts of search_results

--- app/static/js/search_results.js
+++ app/static/js/search_results.js
@@ -70,6 +70,28 @@ var search = function(page,current_div,search_content){
 				var restaurant_id = eachData.restaurant_id;
 				var description = eachData.restaurant_description;
 				
+
+
+
+					// <div class="topics-row-container col-9 d-inline-flex flex-wrap flex-items-center f6 my-1">\
+					// 	<a class="topic-tag topic-tag-link f6 my-1" data-ga-click="Topic, search results" data-octo-click="topic_click" data-octo-dimensions="topic:headers,repository_id:23285482,repository_nwo:helmetjs/&lt;em&gt;csp&lt;/em&gt;,repository_public:true,repository_is_fork:false">\
+					// 	配送费: '+eachData.delivery_fee+'\
+					// 	</a>\
+					// 	<a class="topic-tag topic-tag-link f6 my-1" data-ga-click="Topic, search results" data-octo-click="topic_click" data-octo-dimensions="topic:headers,repository_id:23285482,repository_nwo:helmetjs/&lt;em&gt;csp&lt;/em&gt;,repository_public:true,repository_is_fork:false">\
+					// 	起送费: '+eachData.base_delivery_price+'\
+					// 	</a>\
+					// 	<a class="topic-tag topic-tag-link f6 my-1" data-ga-click="Topic, search results" data-octo-click="topic_click" data-octo-dimensions="topic:headers,repository_id:23285482,repository_nwo:helmetjs/&lt;em&gt;csp&lt;/em&gt;,repository_public:true,repository_is_fork:false">\
+					// 	配送时间: '+eachData.time_span+'\
+					// 	</a>\
+					// 	</div>\
+
+
+
+
+
+						// <p class="col-9 d-inline-block text-gray mb-2 pr-4">起送费: \
+						// '+eachData.base_delivery_price+'\
+						// </p>\
 				alert("discription"+description);
 				alert("address"+address);
 				// alert("restaurant_id"+restaurant_id);
@@ -79,33 +101,19 @@ var search = function(page,current_div,search_content){
 						<h3>\
 						<a href="restaurant_home_page?restaurant_name='+name+'&restaurant_id='+restaurant_id+'&customer_id='+customer_id+'" class="v-align-middle">'+name+'</a>\
 						</h3>\
-						<p class="col-9 d-inline-block text-gray mb-2 pr-4">restaurant_id: \
-						'+restaurant_id+'\
+						<p class="d-inline-block text-gray mb-2 pr-4">'+description+'\
 						</p>\
-						<div class="topics-row-container col-9 d-inline-flex flex-wrap flex-items-center f6 my-1">\
-						<a href="https://github.com/search?q=topic%3Ajavascript+org%3Ahelmetjs&amp;type=Repositories" class="topic-tag topic-tag-link f6 my-1" data-ga-click="Topic, search results" data-octo-click="topic_click" data-octo-dimensions="topic:javascript,repository_id:23285482,repository_nwo:helmetjs/&lt;em&gt;csp&lt;/em&gt;,repository_public:true,repository_is_fork:false">\
-						javascript\
-						</a>\
-						<a href="https://github.com/search?q=topic%3Acsp+org%3Ahelmetjs&amp;type=Repositories" class="topic-tag topic-tag-link f6 my-1" data-ga-click="Topic, search results" data-octo-click="topic_click" data-octo-dimensions="topic:csp,repository_id:23285482,repository_nwo:helmetjs/&lt;em&gt;csp&lt;/em&gt;,repository_public:true,repository_is_fork:false">\
-						csp\
-						</a>\
-						<a href="https://github.com/search?q=topic%3Asecurity+org%3Ahelmetjs&amp;type=Repositories" class="topic-tag topic-tag-link f6 my-1" data-ga-click="Topic, search results" data-octo-click="topic_click" data-octo-dimensions="topic:security,repository_id:23285482,repository_nwo:helmetjs/&lt;em&gt;csp&lt;/em&gt;,repository_public:true,repository_is_fork:false">\
-						security\
-						</a>\
-						<a href="https://github.com/search?q=topic%3Aheaders+org%3Ahelmetjs&amp;type=Repositories" class="topic-tag topic-tag-link f6 my-1" data-ga-click="Topic, search results" data-octo-click="topic_click" data-octo-dimensions="topic:headers,repository_id:23285482,repository_nwo:helmetjs/&lt;em&gt;csp&lt;/em&gt;,repository_public:true,repository_is_fork:false">\
-						headers\
-						</a>\
-						</div>\
-						<p class="f6 text-gray mb-0 mt-2">'+description+'\
-						</p>\
+						<p class="f6 text-gray mb-0 mt-2">'+address+'</p>\
 						</div>\
 						<div class="d-table-cell col-2 text-gray pt-2">\
 						<span class="repo-language-color ml-0" style="background-color:#f1e05a;"></span>\
-						JavaScript\
+						营业时间: '+eachData.open_time+'\
 						</div>\
-						<div class="col-2 text-right pt-1 pr-3 pt-2">\
-						<a class="muted-link" href="https://github.com/helmetjs/csp/stargazers">\
-						<svg aria-label="star" class="octicon octicon-star" height="16" role="img" version="1.1" viewBox="0 0 14 16" width="14"><path fill-rule="evenodd" d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74z"></path></svg>\
+						<div class="col-2 pt-1 pr-3 pt-2">\
+						<a class="muted-link">\
+						<svg aria-label="star" class="octicon octicon-star" height="16" role="img" version="1.1" viewBox="0 0 14 16" width="14">\
+						<path fill-rule="evenodd" d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74z">hehe</path>\
+						</svg>月销量:<br/>'+eachData.total_month_sale+'&nbsp;&nbsp;&nbsp;&nbsp;\
 						</a>\
 						</div>\
 						</div>\

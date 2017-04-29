e7267463e20e6b84619cd1d693f3ae3442c65413
TianxiaoHu <hutianxiao_fdu@126.com>
2017-04-29 21:58:42 +0800

    Huge commit! A demo successfully created!
    - update database
    - user can now change the avatar now:upload_your_profile
    - restaurant can view history: get_restaurant_detail
    - restaurant can delete a dish: delete_dish (change a boolean in
    database)

--- app/views.py
+++ app/views.py
@@ -87,7 +87,7 @@ def user_signup_submit():
         user_exist = g.cursor.execute("SELECT * FROM customer WHERE customer_mobile_number = '%s'" % (customer_mobile_number)).fetchall()
         if user_exist:
             return jsonify({"ERROR": "This mobile has been registered, you can sign in now."})
-        db.engine.execute("INSERT INTO customer VALUES ('%s', '%s', '%s', '%s', NULL, NULL, NULL)" \
+        db.engine.execute("INSERT INTO customer VALUES ('%s', '%s', '%s', '%s', NULL, NULL, NULL, '1')" \
                          % (customer_id,  customer_nickname, customer_password, customer_mobile_number))
         print 'new user inserted into database!'
         return jsonify({"status": 0, "customer_id": customer_id, "customer_nickname": customer_nickname, "customer_mobile_number": customer_mobile_number})
@@ -176,7 +176,7 @@ def jsonify_restaurant(restaurant):
 
 def jsonify_customer(customer):
     key_words = ("customer_id", "customer_nickname", "customer_password", "customer_mobile_number", \
-                 "customer_address", "customer_description", "customer_appellation")
+                 "customer_address", "customer_description", "customer_appellation", "customer_avatar")
     return dict(zip(key_words, customer))
 
 def jsonify_dish(dish):
@@ -220,7 +220,7 @@ def search_dish_results():
     search_value = urllib.unquote(str(search_value))
     print search_value
     try:
-        result = g.cursor.execute("SELECT dish_id, dish_name, dish.restaurant_id, dish_price, dish_month_sale, restaurant_name FROM dish, restaurant WHERE dish_name LIKE '%%%s%%' AND dish.restaurant_id = restaurant.restaurant_id" % (search_value)).fetchall()
+        result = g.cursor.execute("SELECT dish_id, dish_name, dish.restaurant_id, dish_price, dish_month_sale, restaurant_name FROM dish, restaurant WHERE dish_name LIKE '%%%s%%' AND dish.restaurant_id = restaurant.restaurant_id AND NOT deleted" % (search_value)).fetchall()
         print result
         total_result = len(result)
         total_page = total_result / PAGINATION_PER_PAGE + 1
@@ -254,8 +254,9 @@ def upload_your_profile():
     customer_address = request.args.get("customer_address")
     customer_description = request.args.get("customer_description")
     customer_appellation = request.args.get("customer_appellation")
+    customer_avatar = request.args.get("customer_avatar")
     try:
-        db.engine.execute("UPDATE customer SET customer_nickname = '%s', customer_address = '%s', customer_description = '%s', customer_appellation = '%s' WHERE customer_id = '%s'" %(customer_nickname, customer_address, customer_description, customer_appellation, customer_id))
+        db.engine.execute("UPDATE customer SET customer_nickname = '%s', customer_address = '%s', customer_description = '%s', customer_appellation = '%s', customer_avatar = '%s' WHERE customer_id = '%s'" %(customer_nickname, customer_address, customer_description, customer_appellation, customer_avatar, customer_id))
         print 'successfully updated profile!'
         updated_profile = g.cursor.execute("SELECT * FROM customer WHERE customer_id = '%s'" % (customer_id)).fetchall()
         return jsonify(jsonify_customer(updated_profile[0]))
@@ -309,7 +310,7 @@ def get_restaurant_detail():
         restaurant = g.cursor.execute("SELECT * FROM restaurant WHERE restaurant_id = '%s'" % (restaurant_id)).fetchall()
         if restaurant:
             dish_list = []
-            dish_result = g.cursor.execute("SELECT  dish_id, dish_name, dish.restaurant_id, dish_price, dish_month_sale FROM dish, restaurant WHERE dish.restaurant_id = restaurant.restaurant_id AND restaurant.restaurant_id = '%s'" % (restaurant_id)).fetchall()
+            dish_result = g.cursor.execute("SELECT  dish_id, dish_name, dish.restaurant_id, dish_price, dish_month_sale FROM dish, restaurant WHERE dish.restaurant_id = restaurant.restaurant_id AND restaurant.restaurant_id = '%s' AND NOT deleted" % (restaurant_id)).fetchall()
             print dish_result
             for dish in dish_result:
                 dish_list.append(jsonify_dish(dish))
@@ -357,8 +358,43 @@ def get_user_history():
 
 @app.route('/get_restaurant_history', methods=['GET', 'POST'])
 def get_restaurant_history():
-    # TODO
-    pass
+    """
+    :return: json
+    [{"customer_nickname":customer_nickname, "create_time":create_time, "receive_time":receive_time,
+     "comment":comment, "order_total_price":order_total_price, 
+     "dishes":[{"dish_price": dish_price, "dish_amount": dish_amount, "dish_name":dish_name}, {}, {}..]
+     },
+     {}, {}, ...]
+    """
+    restaurant_id = request.args.get("restaurant_id")
+    print restaurant_id
+    try:
+        restaurant_order = g.cursor.execute("SELECT customer_order.customer_id, customer_order.order_id, customer_order.create_time, customer_order.receive_time, customer_order.comment FROM customer_order WHERE restaurant_id = '%s'" % (restaurant_id)).fetchall()
+        if restaurant_order:
+            order_list = []
+            for order in restaurant_order:
+                print order
+                keywords = ["customer_nickname", "order_id", "create_time", "receive_time", "comment"]
+                order_dict = dict(zip(keywords, order))
+                customer_nickname = g.cursor.execute("SELECT customer_nickname FROM customer WHERE customer_id = '%s'" % (order[0])).fetchall()
+                order_dict["customer_nickname"] = customer_nickname[0][0]
+                dish_details = g.cursor.execute("SELECT dish.dish_name, dish.dish_price, dish_order.count FROM dish, dish_order, customer_order WHERE customer_order.order_id = dish_order.order_id AND dish_order.dish_id = dish.dish_id AND customer_order.order_id = '%s'" % (order_dict["order_id"])).fetchall()
+                order_total_price = sum(map(lambda x: x[1] * x[2], dish_details))
+                order_dict["order_total_price"] = order_total_price
+                dish_list = []
+                for dish in dish_details:
+                    dish_list.append(dict(zip(("dish_name", "dish_price", "dish_amount"), dish)))
+                order_dict["dishes"] = dish_list
+                print order_dict
+                order_list.append(order_dict)
+            print order_list
+            return jsonify({"result":order_list})
+        else:
+            return jsonify({"ERROR": "No orders! Your business is so poor.."})
+    except Exception as e:
+        print 'get history failed!'
+        print traceback.format_exc(e)
+        return jsonify({"ERROR": "Get restaurant history failed, please try again later.."})
 
 def get_customer_order_no():
     total_customer_order_num = len(db.engine.execute("SELECT * FROM customer_order").fetchall())
@@ -453,8 +489,15 @@ def receive_order():
 
 @app.route('/delete_dish', methods=['GET', 'POST'])
 def delete_dish():
-    # TODO
-    pass
+    dish_id = request.args.get("dish_id")
+    try:
+        db.engine.execute("UPDATE dish SET deleted = 1 WHERE dish_id = '%s'" % (dish_id))
+        print "dish marked as `deleted`!"
+        return jsonify({"succeed!": "succeed!"})
+    except Exception as e:
+        print 'dish delete failed!'
+        print traceback.format_exc(e)
+        return jsonify({"ERROR": "Dish delete failed, please try again later.."})
 
 @app.route('/comment_order', methods=['GET', 'POST'])
 def comment_order():
